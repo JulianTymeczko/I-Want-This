cf2837f771ad6a7e81225265397a91a8beee7511
Julian Houle-T <jhouletymeczko@gmail.com>
2023-10-02 20:44:12 -0400

    successfully imported bootstrap

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <img src="/assets/react.svg" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 }
