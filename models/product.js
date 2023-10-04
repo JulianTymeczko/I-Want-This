044688a52ab64716f4f3ff2bfad84d47a08a993f
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-04 19:44:49 -0400

    created models folder and category.js, index.js, order.js, product.js and user.js

---
+++ models/product.js
@@ -0,0 +1,27 @@
+// products.js
+const products = [
+    {
+      id: 1,
+      name: "Cupcake",
+      category: "cupcakes",
+      flavors: ["strawberry", "vanilla", "chocolate", "marble", "red-velvet"],
+      price: 2.99,
+    },
+    {
+      id: 2,
+      name: "Cake",
+      category: "cakes",
+      flavors: ["red-velvet", "vanilla", "chocolate", "strawberry", "marble"],
+      price: 22.99,
+    },
+    {
+      id: 3,
+      name: "Donut",
+      category: "donuts",
+      flavors: ["vanilla", "chocolate", "strawberry", "marble", "red-velvet"],
+      price: 1.49,
+    },
+    // Add more products here
+  ];
+  
+  module.exports = products;
