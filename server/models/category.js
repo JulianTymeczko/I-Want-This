8bf197ef8dcac0988a5b6cb0130956a47c5db6a4
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-04 20:24:13 -0400

    updated Category.js file

--- server/models/category.js
+++ server/models/category.js
@@ -1,8 +1,17 @@
-// category.js
-const categories = {
-    cakes: "Cakes",
-    cupcakes: "Cupcakes",
-    donuts: "Donuts",
-  };
-  
-  module.exports = categories;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2, // Add minimum length
+    maxlength: 50, // Add maximum length
+  }
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category
