74e24c36f3b41356d9f925948913d48b47dcb0e5
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-05 18:57:51 -0400

    added auth.js and some server.js code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // required packages
 const express = require('express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 // Import the ApolloServer class
 const { ApolloServer } = require('apollo-server-express');
@@ -11,13 +13,25 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  //context: authMiddleware
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
