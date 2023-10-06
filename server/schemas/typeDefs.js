fe1b8999010e67e8c7aee672bf92546210ce010c
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-05 20:48:30 -0400

    updated typeDefs.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -54,19 +54,23 @@ type Category {
   }
 
   """ ---Queries--- """
-  {
+  
+  type Query {
     categories: [Category]
-    products:(category: ID, name: String, description: String, price: Float): [Product]
+    products(category: ID, name: String, description: String, price: Float): [Product]
     product(_id: ID): Product
     user: User
     order(_id: ID!): Order
     checkout(products: [ID]!): Checkout
-
   }
 
   """ ---Mutations--- """
-  {
-
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
   }
   `;
 
