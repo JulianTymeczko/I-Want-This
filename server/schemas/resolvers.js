221f39e556fcb22e07231f6d2fdd04bfb5e61446
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-05 21:00:40 -0400

    added query code to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,31 +1,72 @@
 const { Category, Order, Product, User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         // Category Query
-      category: async () => {
+      categories: async () => {
         return await Category.find();
       },
 
-       // Order Query
-       order: async () => {
-        return await Order.find();
+      // Products Query
+      products: async (parent, { category, name }) => {
+        const params = {};
+  
+        if (category) {
+          params.category = category;
+        }
+  
+        if (name) {
+          params.name = {
+            $regex: name
+          };
+        }
+  
+        return await Product.find(params).populate('category');
       },
 
       // Product Query
-      product: async () => {
-        return await Product.find();
+      product: async (parent, { _id }) => {
+        return await Product.findById(_id).populate('category');
+      },
+
+       // Order Query
+       order: async (parent, { _id }, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: 'orders.products',
+            populate: 'category'
+          });
+  
+          return user.orders.id(_id);
+        }
+  
+        throw new AuthenticationError('Not logged in');
       },
 
 
       // User Query
-      user: async () => {
-        return await User.find();
+      user: async (parent, args, context) => {
+        if (context.user) {
+          const user = await User.findById(context.user._id).populate({
+            path: 'orders.products',
+            populate: 'category'
+          });
+  
+          user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+  
+          return user;
+        }
+  
+        throw new AuthenticationError('Not logged in');
       },
   
+      // Checkout Query
     },
 
     // add mutations
+    
 
 
 
