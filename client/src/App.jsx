66806e7ca03a95dbb6c0db3ba1600f4f4ee0b6ce
Julian Houle-T <jhouletymeczko@gmail.com>
2023-10-07 22:11:20 -0400

    fixed multiple bugs involving adding to the cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,32 +11,36 @@ export const FilterContext = createContext();
 export const VanillaContext = createContext();
 export const StrawberryContext = createContext();
 export const ChocolateContext = createContext();
+export const DefaultContext = createContext();
 export default function App() {
   const [checkout, setCheckout] = useState([]);
   const [filter, setFilter] = useState("");
   const [vanilla, setVanilla] = useState(false);
   const [strawberry, setStrawberry] = useState(false);
   const [chocolate, setChocolate] = useState(false);
+  const [defaultValue, setDefaultValue] = useState(0);
   return (
     <>
       <Router>
-        <VanillaContext.Provider value={{ vanilla, setVanilla }}>
-          <StrawberryContext.Provider value={{ strawberry, setStrawberry }}>
-            <ChocolateContext.Provider value={{ chocolate, setChocolate }}>
-              <FilterContext.Provider value={{ filter, setFilter }}>
-                <Nav></Nav>
-                <CheckoutContext.Provider value={{ checkout, setCheckout }}>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                  </Routes>
-                </CheckoutContext.Provider>
-              </FilterContext.Provider>
-            </ChocolateContext.Provider>
-          </StrawberryContext.Provider>
-        </VanillaContext.Provider>
+        <DefaultContext.Provider value={{ defaultValue, setDefaultValue }}>
+          <VanillaContext.Provider value={{ vanilla, setVanilla }}>
+            <StrawberryContext.Provider value={{ strawberry, setStrawberry }}>
+              <ChocolateContext.Provider value={{ chocolate, setChocolate }}>
+                <FilterContext.Provider value={{ filter, setFilter }}>
+                  <Nav></Nav>
+                  <CheckoutContext.Provider value={{ checkout, setCheckout }}>
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/store" element={<Store />} />
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/signup" element={<SignUp />} />
+                    </Routes>
+                  </CheckoutContext.Provider>
+                </FilterContext.Provider>
+              </ChocolateContext.Provider>
+            </StrawberryContext.Provider>
+          </VanillaContext.Provider>
+        </DefaultContext.Provider>
       </Router>
     </>
   );
