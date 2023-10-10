f7724381ba1fcee6b01a20a1497b38c6035d9375
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-10 12:50:48 -0400

    addressing error in Models

--- server/models/index.js
+++ server/models/index.js
@@ -1,16 +1,18 @@
 // index.js
-const Category = require("category");
-const Product = require("product");
-const Order = require("order");
-const User = require("user");
+const Category = require("./category");
+const Product = require("./product");
+const Order = require("./order");
+const User = require("./user");
 
 // Example usage
+/*
 console.log("Categories:", categories);
 console.log("Products:", products);
 const cart = [products[0], products[1]];
 console.log("Total Cost:", order.calculateTotalCost(cart));
 const foundUser = user.findUserByUsername("user1");
 console.log("Found User:", foundUser);
+*/
 
 module.exports = {
     Category, 
