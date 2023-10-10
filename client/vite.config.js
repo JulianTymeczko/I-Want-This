c33cf3aaa9f95faceb120c29b485cc28705932b2
SMarquis85 <sophienathalie_85@outlook.com>
2023-10-10 12:43:47 -0400

    update vite config with proxy target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/  --> localhost:5713
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://locahost:3001',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
 })
